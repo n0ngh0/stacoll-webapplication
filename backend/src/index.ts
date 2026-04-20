9243e9e7c147021aafd9bdcda33dd57f1c87d5d9
n0ngh0..//! <nonghoeiei123@gmail.com>
2026-04-20 16:31:45 +0700

    Update backend index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,10 @@
+import { configDotenv } from "dotenv";
 import { Elysia } from "elysia";
+configDotenv()
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const port = process.env.PORT || 3000
+
+const app = new Elysia().get("/", () => "Hello Elysia").listen(port);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
